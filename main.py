3786a664cf2419fd8837fd2a91c261a8b8e84a34
mishalhossin <mishalhossin20@gmail.com>
2023-08-20 21:24:55 +0600

    Refactor code

--- main.py
+++ main.py
@@ -5,9 +5,7 @@ import discord
 import hashlib
 import random
 import json
-import asyncio
 import requests
-import uuid
 from discord.ext import commands
 from dotenv import load_dotenv
 
@@ -17,10 +15,27 @@ load_dotenv()
 intents = discord.Intents.default()
 bot = commands.Bot(command_prefix="/", intents=intents, heartbeat_timeout=60)
 TOKEN = os.getenv('DISCORD_TOKEN')
+media_content_types = [
+    'image/jpeg',
+    'image/png',
+    'image/gif',
+    'video/mp4',
+    'video/quicktime',
+    'video/x-msvideo',
+    'video/x-matroska'
+]
 
 def print_in_color(text, color):
     print(f"\033[{color}m{text}\033[0m")
-    
+
+def load_responses(url):
+    response = json.loads(requests.get(url).content)["responses"]
+    return response
+
+responses = load_responses(
+    "https://gist.githubusercontent.com/mishalhossin/dd2296aa6c5fb1518df2bd5266193594/raw/41e69dd4b53d691d22839da5e5e4ea36440aba7e/responses.json"
+)
+
 @bot.event
 async def on_ready():
     await bot.tree.sync()
@@ -30,41 +45,31 @@ async def on_ready():
         scopes=("bot", "applications.commands")
     )
 
-    if os.name == 'posix':
-        os.system('clear')
-    if os.name == 'nt':
-        os.system('cls')
+    os.system('clear' if os.name == 'posix' else 'cls')
     
-    print_in_color(f"{bot.user} is ready to upload some files!", "\033[1;97")
+    print_in_color(f"{bot.user} is ready to upload some files!", "1;97")
     print_in_color(f"Invite link: {invite_link}", "1;36")
-
-
-def load_responses(url):
-    response = json.loads(requests.get(url).content)["responses"]
-    return response 
-    
-responses = load_responses("https://gist.githubusercontent.com/mishalhossin/dd2296aa6c5fb1518df2bd5266193594/raw/41e69dd4b53d691d22839da5e5e4ea36440aba7e/responses.json")
-print(f"Loaded {len(responses)} responses")
 
 @bot.hybrid_command(name="upload", description="Upload a file")
 async def upload(ctx, attachment: discord.Attachment):
     await ctx.defer()
     random_msg = random.choice(responses)
-    message = await ctx.send(f"Selling your soul to the devil..")
+    message = await ctx.send("Selling your soul to the devil..")
     file_data = await attachment.read()
     file_name = f"{attachment.filename}"
-    print_in_color(f"Uploading file {file_name}", "\033[32")
-    await message.edit(content=f"Calculating file hash")
+    file_type = file_name.split(".")[-1] if "." in file_name else "Unknown"
+    print_in_color(f"Uploading file {file_name}", "32")
+    await message.edit(content="Calculating file hash")
     hash_value = hashlib.sha256(file_data).hexdigest()
     await message.edit(content=f"File hash: {hash_value}")
     bytes_io = io.BytesIO(file_data)
-    await message.edit(content=f"Calculating file bytes size ")
+    await message.edit(content="Calculating file bytes size ")
     bytes_size = len(bytes_io.getbuffer())
     await message.edit(content=f"File bytes size: {bytes_size}")
     file = (file_name, bytes_io)
 
     try:
-        await message.edit(content=f"Trying to upload file...")
+        await message.edit(content="Trying to upload file...")
         
         data = aiohttp.FormData()
         data.add_field('file', file[1], filename=file[0])
@@ -73,24 +78,27 @@ async def upload(ctx, attachment: discord.Attachment):
             async with session.post("https://0x0.st", data=data) as response:
                 if response.status == 200:
                     file_url = await response.text()
-                    is_nsfw = False
-                    footer_text = random_msg
-                    color = discord.Color.random()
-                    if "#nsfw" in file_url:
-                        color = 0xff0000
-                        footer_text = "⚠️ The uploaded file has been detected to contain content that is not suitable for a work environment or public display. Please be cautious when viewing"
-                        is_nsfw = True
-                        
-                    embed = discord.Embed(title="Upload Result", color=color)
+                    is_nsfw = "#nsfw" in file_url
+                    color = 0xff0000 if is_nsfw else discord.Color.random()
+                    footer_text = random_msg if is_nsfw else random_msg
+                    description = ""
+                    preview_length = 100
+                    file_preview = file_data.decode(errors="ignore")[:preview_length]
+                    if attachment.content_type not in media_content_types:
+                        description = f"```{file_type}\n{file_preview}[...]```"
+
+                    embed = discord.Embed(title="Upload Result", description=description, color=color)
                     embed.add_field(name="File Hash (sha256)", value=hash_value, inline=False)
-                    embed.add_field(name="File Name", value=file_name, inline=False)
-                    embed.add_field(name="File Size", value=f"{bytes_size} bytes", inline=False)
+                    embed.add_field(name="File Name", value=file_name, inline=True)
+                    embed.add_field(name="File Type", value=file_type, inline=True)
+                    embed.add_field(name="File Size", value=f"{bytes_size} bytes", inline=True)
                     if is_nsfw:
                         embed.add_field(name="0x0 File URL", value=f"||{file_url}||", inline=False)
                     else:
                         embed.add_field(name="0x0 File URL", value=file_url, inline=True)
                         embed.set_image(url=attachment.url)
                     embed.set_footer(text=footer_text)
+                    
                     view = discord.ui.View()
                     view.add_item(discord.ui.Button(label="Open File", url=file_url))
                     view.add_item(discord.ui.Button(label="Virus Check", url=f"https://www.virustotal.com/gui/search/{hash_value}"))
@@ -104,6 +112,6 @@ async def upload(ctx, attachment: discord.Attachment):
         embed = discord.Embed(title="Upload failed", color=0xff0000)
         embed.add_field(name="Error Message", value=str(e), inline=False)
         await message.edit(content="", embed=embed)
-                
+
 if __name__ == "__main__":
     bot.run(TOKEN)
